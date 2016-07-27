61ddce58e0401d28b5409f21bb7542f4554fd5a5
Florian Abele <falknor1988@gmail.com>
2016-07-27 17:42:42 +0200

    First Try Implementation SuffixTree

--- testMain.cpp
+++ testMain.cpp
@@ -1,8 +1,25 @@
 
 #include <iostream>
+#include "SuchenStatischenTexten/SufTree.h"
+#include "SuchenStatischenTexten/TextWord.h"
 
 int main(int argc, char const *argv[])
 {
   std::cout << "Travis make Test successful." << std::endl;
+
+  TextWord test("blub");
+
+  if (test.exactMatch("b")) {
+    std::cout << "found b !!! error" << std::endl;
+  }
+  if (test.exactMatch("xlub")) {
+    std::cout << "found xlub!!! error" << std::endl;
+  }
+  if (test.exactMatch("blub")) {
+    std::cout << "found blub" << std::endl;
+  }
+  test.status();
+
+
   return 0;
 }
