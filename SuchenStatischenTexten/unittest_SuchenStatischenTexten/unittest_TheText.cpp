578af9e77634cd3104482cb851e456d9a10186db
Tim M <Tim M>
2016-08-02 17:59:20 +0200

    added unittest_TheText.cpp (geht noch nicht der shit)

---
+++ SuchenStatischenTexten/unittest_SuchenStatischenTexten/unittest_TheText.cpp
@@ -0,0 +1,7 @@
+//#include <gtest/gtest.h>
+//#include "../SuchenStatischenTexten/TheText.h"
+//
+//TEST(Unittest_TheText, ExactMatchCount){
+//	TheText text("unittest_TheTest.cpp");
+//	EXPECT_EQ(6, text.exactMatchCount("Text"));
+//}
